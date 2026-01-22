483977ac42237253c8a3e2fe04fc48c67e844d21
{Anshu} <{anshu2711@gmail.com}>
2026-01-22 16:21:56 +0530

    assignment2

--- examples/assignment2.js
+++ examples/assignment2.js
@@ -1,2 +1,4 @@
-create a js script object named user that meets the following:
-property1 (age): a no. representing an age
+// create a js script object named user that meets the following:
+// property1 (age): a no. representing an age (eg. 25)
+// property2(name):Fnxn (method) that takes no Parameter
+// task: inside the name Fnxn, write a statement that accesses and prints the age property of the object to the console
