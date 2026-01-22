51d55eb93e375379188c7486dcd9ce21ce2703d9
{Anshu} <{anshu2711@gmail.com}>
2026-01-22 10:51:29 +0530

    adding arrow as a function

--- examples/example10.js
+++ examples/example10.js
@@ -6,7 +6,17 @@
 // const r=add(10,20);
 // console.log(r);
 
-const add=(a,b)=>a+b;
 
+const add=(a,b)=>a+b;  //if we have to write one statement, no need of curly braces
+const add_2=(a,b)=>{  //if more than one statement, then use curly braces
+    a=a+2;
+    b=b+2;
+    return a+b;
+}
+const square=a=>a*a; //if one parameter, no need of braces
+
+const x=add_2(10,20);
 const r=add(10,20);
+console.log(square(10));
 console.log(r);
+console.log(x);
